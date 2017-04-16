eaf2add86073c87e1a0b1a382d96fb0c08ba99e6
Roger Wakeman <rgrwkmn@gmail.com>
2017-04-16 11:51:02 -0500

    Better error message when no map is specified

--- src/scripts/levelTester/state.js
+++ src/scripts/levelTester/state.js
@@ -4,7 +4,8 @@ class State extends TiledLevelState {
   init() {
     const map = window.location.search.match(/map=([^&]+)/);
     if (!map) {
-      console.error(`Can't load map from url path: ${window.location.href}. Looking for ?map=mapFolder/mapJson.json`);
+      console.error(`YO YO YO: can't load map from url path: ${window.location.href}. Looking for ?map=mapFolder/mapJson.json`);
+      console.error('Try http://localhost:8001/?map=larger/larger.json');
     }
     const mapPath = `/assets/maps/${map[1]}`;
     console.log(`Loading map ${mapPath}`);
